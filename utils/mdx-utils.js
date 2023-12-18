c2f4df08e0cac6b9449bd4a06eca02e72df60792
Jhones <jhonesaly@gmail.com>
2023-12-18 17:15:07 -0300

    feat: atualizações

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -1,19 +1,15 @@
 import { api } from '../services/api';
 
 export const getPosts = async () => {
-  try {
-    const { data } = await api.get('/posts');
-
-    if (data) {
+  const {data} = await api.get('/posts'); 
+  
+  if(data){
+      console.log(data)
       return data;
-    }
-
-    return [];
-  } catch (error) {
-    console.error('Erro ao obter posts:', error);
-    return [];
   }
-};
+
+  return []
+}
 
 export const getPostBySlug = async (id) => {
   try {
