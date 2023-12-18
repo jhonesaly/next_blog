1d331bafd356a8fbb8368320648dafe24b432e7c
Jhones <jhonesaly@gmail.com>
2023-12-18 15:42:00 -0300

    feat: api service

---
+++ services/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const api = axios.create({
+    baseURL: 'https://inrhxwfbuogxeabggrjw.supabase.co/rest/v1',
+    headers: {
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlucmh4d2ZidW9neGVhYmdncmp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI5MjE4MzgsImV4cCI6MjAxODQ5NzgzOH0.1TWQ6ZfeQ7sYcxeqm0Ezi0GE9MblxhrzCe287YA7Uts",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlucmh4d2ZidW9neGVhYmdncmp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI5MjE4MzgsImV4cCI6MjAxODQ5NzgzOH0.1TWQ6ZfeQ7sYcxeqm0Ezi0GE9MblxhrzCe287YA7Uts"
+    }
+})
